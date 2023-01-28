015cb8784c4ecec901b03f11c9d59f80a62ad5b2
dvdfl <flo22001@byui.edu>
2023-01-27 21:34:24 -0500

    Completed week 4 assignment

--- w02/cms/src/app/documents/document-list/document-list.component.ts
+++ w02/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  
+  documents : Document[] = [
+    new Document("1", "Summary", "Contains a summary of  lesson # 4", "https://duckduckgo.com/?q=Summary", null),
+    new Document("2", "Homework", "homework information of lesson # 4", "https://duckduckgo.com/?q=Homework", null),
+    new Document("3", "CSE341-Lesson 4: API Documentation", "The most recent version of my resume document", "https://cse341.netlify.app/lesson4", null),
+    new Document("4", "CSE341 - Project Requirements", "Essay homework of lesson # 4", "https://cse341.netlify.app/projects/1", null),
+    new Document("5", "Study Guide", "First 4 units study guide", "https://duckduckgo.com/?q=Study+Guide", null)
+  ];
 
+  onSelectedDocument(document:Document){
+    //console.log(document)
+    this.selectedDocumentEvent.emit(document);
+  }
 }
