6c8675dc60dfc16b59dd49a02efacfd88ce65d98
dvdfl <flo22001@byui.edu>
2023-02-01 22:57:28 -0500

    implemented document service

--- w02/cms/src/app/documents/document-list/document-list.component.ts
+++ w02/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,24 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent {
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
-  
-  documents : Document[] = [
-    new Document("1", "Summary", "Contains a summary of  lesson # 4", "https://duckduckgo.com/?q=Summary", null),
-    new Document("2", "Homework", "homework information of lesson # 4", "https://duckduckgo.com/?q=Homework", null),
-    new Document("3", "CSE341-Lesson 4: API Documentation", "The most recent version of my resume document", "https://cse341.netlify.app/lesson4", null),
-    new Document("4", "CSE341 - Project Requirements", "Essay homework of lesson # 4", "https://cse341.netlify.app/projects/1", null),
-    new Document("5", "Study Guide", "First 4 units study guide", "https://duckduckgo.com/?q=Study+Guide", null)
-  ];
+export class DocumentListComponent implements OnInit{
+  //@Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents : Document[] = [];
+  // documents : Document[] = [
+  //   new Document("1", "Summary", "Contains a summary of  lesson # 4", "https://duckduckgo.com/?q=Summary", null),
+  //   new Document("2", "Homework", "homework information of lesson # 4", "https://duckduckgo.com/?q=Homework", null),
+  //   new Document("3", "CSE341-Lesson 4: API Documentation", "The most recent version of my resume document", "https://cse341.netlify.app/lesson4", null),
+  //   new Document("4", "CSE341 - Project Requirements", "Essay homework of lesson # 4", "https://cse341.netlify.app/projects/1", null),
+  //   new Document("5", "Study Guide", "First 4 units study guide", "https://duckduckgo.com/?q=Study+Guide", null)
+  // ];
+
+  constructor(private documentService: DocumentService){ }
+
+  ngOnInit(): void {
+    this.documents = this.documentService.documents.slice();
+  }
 
   onSelectedDocument(document:Document){
     //console.log(document)
-    this.selectedDocumentEvent.emit(document);
+    //this.selectedDocumentEvent.emit(document);
+    this.documentService.documentSelectedEvent.emit(document);
   }
 }
--- w02/cms/src/app/documents/documents.component.ts
+++ w02/cms/src/app/documents/documents.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from './document.model';
+import { DocumentService } from './document.service';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent {
-  selectedDocument : Document
+export class DocumentsComponent implements OnInit {
+  selectedDocument : Document;
+
+  constructor(private documentservice :DocumentService){ }
+  
+  ngOnInit(): void {
+    this.documentservice.documentSelectedEvent.subscribe((documewnt:Document)=>{
+      this.selectedDocument = documewnt;
+      console.log("new subscribe")
+    });
+  }
 }
