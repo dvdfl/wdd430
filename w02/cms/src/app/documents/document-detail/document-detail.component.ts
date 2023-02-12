0e38187bd08805ccac03b65a903a45b46568291b
dvdfl <flo22001@byui.edu>
2023-02-11 21:56:31 -0500

    document routes implemented

--- w02/cms/src/app/documents/document-detail/document-detail.component.ts
+++ w02/cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,10 +1,39 @@
-import { Component, Input } from '@angular/core';
-import { Document } from '../document.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DocumentService } from '../document.service';
+import {Document} from '../document.model';
+import { WindRefService } from 'src/app/wind-ref.service';
 @Component({
   selector: 'cms-document-detail',
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.css']
 })
-export class DocumentDetailComponent {
-  @Input() document: Document;
+export class DocumentDetailComponent implements OnInit {
+  document: Document;
+  nativeWindow: any;
+  constructor(private documentService: DocumentService, 
+    private router : Router, private route : ActivatedRoute,
+    private windRefService: WindRefService ) {
+
+  }
+  ngOnInit(): void {
+    // let docId = this.route.snapshot.params['id'];
+    // this.document = this.documentService.getDocument(docId);
+    // console.log(this.document);
+    this.nativeWindow = this.windRefService.getNativeWindow();
+    this.route.params.subscribe(
+      (params)=>{
+        let docId = params['id'];
+        this.document = this.documentService.getDocument(docId);
+        //console.log(this.document);
+      }
+    )
+  }
+  onView(){
+    this.nativeWindow.open(this.document.url);
+  }
+  onDelete(){
+    this.documentService.deleteDocument(this.document);
+    this.router.navigate(["/documents"]);
+  }
 }
--- w02/cms/src/app/documents/document-list/document-list.component.ts
+++ w02/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
 
@@ -8,25 +8,13 @@ import { DocumentService } from '../document.service';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit{
-  //@Output() selectedDocumentEvent = new EventEmitter<Document>();
+  
   documents : Document[] = [];
-  // documents : Document[] = [
-  //   new Document("1", "Summary", "Contains a summary of  lesson # 4", "https://duckduckgo.com/?q=Summary", null),
-  //   new Document("2", "Homework", "homework information of lesson # 4", "https://duckduckgo.com/?q=Homework", null),
-  //   new Document("3", "CSE341-Lesson 4: API Documentation", "The most recent version of my resume document", "https://cse341.netlify.app/lesson4", null),
-  //   new Document("4", "CSE341 - Project Requirements", "Essay homework of lesson # 4", "https://cse341.netlify.app/projects/1", null),
-  //   new Document("5", "Study Guide", "First 4 units study guide", "https://duckduckgo.com/?q=Study+Guide", null)
-  // ];
-
   constructor(private documentService: DocumentService){ }
 
   ngOnInit(): void {
     this.documents = this.documentService.documents.slice();
+    this.documentService.documentChangedEvent.subscribe((docs)=>this.documents = docs)
   }
-
-  onSelectedDocument(document:Document){
-    //console.log(document)
-    //this.selectedDocumentEvent.emit(document);
-    this.documentService.documentSelectedEvent.emit(document);
-  }
+  
 }
