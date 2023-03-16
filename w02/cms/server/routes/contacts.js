8c4c63af55737fe0f4a383dd05eaf43f1ed980ad
dvdfl <flo22001@byui.edu>
2023-03-15 23:26:30 -0400

    backend changes models and methods added

--- w02/cms/server/routes/contacts.js
+++ w02/cms/server/routes/contacts.js
@@ -1,9 +1,102 @@
 var express = require('express');
 var router = express.Router();
+const sequenceGenerator = require('./sequenceGenerator');
+const Contact = require('../models/contact');
 
 /* GET all contacts. */
 router.get('/', function(req, res, next) {
-  res.send("Contacts GET enpoint working!");
+  //res.send("Contacts GET enpoint working!");
+  Contact.find().populate('group')
+  .then(contacts=>{
+    res.status(200).send(contacts);
+  })
+  .catch(ex=>{
+    console.log(ex);
+    res.status(500).json({title: "An error occured", error: ex})
+  })
 });
+
+router.post('/', (req, res, next) => {
+  const maxContactId = sequenceGenerator.nextId("contacts");
+
+  const contact = new Contact({
+    id: maxContactId,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.bodyphone,
+    imageUrl: req.body.imageUrl,
+    contacts: req.body.contacts
+  });
+
+  contact.save()
+    .then(createdContact => {
+      res.status(201).json({
+        message: 'Contact added successfully',
+        contact: createdContact
+      });
+    })
+    .catch(error => {
+       res.status(500).json({
+          message: 'An error occurred',
+          error: error
+        });
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  Contact.findOne({ id: req.params.id })
+    .then(contact => {
+
+      contact.name = req.body.name;
+      contact.email = req.body.email;
+      contact.phone = req.body.phone;
+      contact.imageUrl = req.body.imageUrl;
+      contact.contacts = req.body.contacts;
+
+      Contact.updateOne({ id: req.params.id }, contact)
+        .then(result => {
+          res.status(204).json({
+            message: 'Contact updated successfully'
+          })
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Contact not found.',
+        error: { contact: 'Contact not found'}
+      });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Contact.findOne({ id: req.params.id })
+    .then(contact => {
+      Contact.deleteOne({ id: req.params.id })
+        .then(result => {
+          res.status(204).json({
+            message: "Contact deleted successfully"
+          });
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        })
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Contact not found.',
+        error: { contact: 'Contact not found'}
+      });
+    });
+});
+
 
 module.exports = router;
--- w02/cms/server/routes/messages.js
+++ w02/cms/server/routes/messages.js
@@ -1,9 +1,99 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sequenceGenerator = require('./sequenceGenerator');
+const Message = require('../models/message');
+const message = require('../models/message');
+
 
 /* GET all messages. */
 router.get('/', function(req, res, next) {
-  res.send("Messages GET enpoint working!");
+  //res.send("Messages GET enpoint working!");
+  Message.find().then(messages=>{
+    res.status(200).send(messages);
+  })
+  .catch(ex=>{
+    console.log(ex);
+    res.status(500).json({ title : "An Error occurred", error : ex });
+  })
 });
+
+router.post('/', (req, res, next) => {
+  const maxMessageId = sequenceGenerator.nextId("messages");
+
+  const message = new Message({
+    id: maxMessageId,
+    subject: req.body.subject,
+    msgText: req.body.msgText,
+    sender: req.body.sender,
+  });
+
+  message.save()
+    .then(createdMessage => {
+      res.status(201).json({
+        message: 'Message added successfully',
+        newMessage: createdMessage
+      });
+    })
+    .catch(error => {
+       res.status(500).json({
+          message: 'An error occurred',
+          error: error
+        });
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+
+      message.subject = req.body.subject;
+      message.msgText= req.body.msgText;
+      message.sender= req.body.sender;
+
+      Message.updateOne({ id: req.params.id }, message)
+        .then(result => {
+          res.status(204).json({
+            message: 'Message updated successfully'
+          })
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Message not found.',
+        error: { message: 'Message not found'}
+      });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+      Message.deleteOne({ id: req.params.id })
+        .then(result => {
+          res.status(204).json({
+            message: "Message deleted successfully"
+          });
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        })
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Message not found.',
+        error: { message: 'Message not found'}
+      });
+    });
+});
+
 
 module.exports = router;
